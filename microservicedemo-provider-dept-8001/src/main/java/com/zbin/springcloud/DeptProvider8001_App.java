fcc10927f0e4d15c7d0b704006061df03242c65c
Zbin <wuzhibin@qipeng.com>
2019-03-14 15:49:45 +0800

    搭建eureka集群

--- microservicedemo-provider-dept-8001/src/main/java/com/zbin/springcloud/DeptProvider8001_App.java
+++ microservicedemo-provider-dept-8001/src/main/java/com/zbin/springcloud/DeptProvider8001_App.java
@@ -2,6 +2,7 @@ package com.zbin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * com.zbin.springcloud.DeptProvider8001_App
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/03/13
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
 
   public static void main(String[] args) {
